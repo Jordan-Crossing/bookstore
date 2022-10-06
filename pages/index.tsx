f506c0f70298950f4626ef3b8b6a8b991deebffe
Jordan-Crossing <80609165+Jordan-Crossing@users.noreply.github.com>
2022-10-07 00:05:12 +1100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,75 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+    <div className="text-center py-4 px-4">
+      <h1 className="text-sky-400">BookStore</h1>
+      <p>Submit your book ideas</p>
+      <form
+        action="#"
+        method="POST"
+        className=" grid grid-cols1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+      >
+        <div>
+          <label
+            htmlFor="book-title"
+            className=" text-sm font-medium text-grey-700"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            Book Title
+          </label>
+          <input
+            type="text"
+            name="book-title"
+            id="book-title"
+            autoComplete="given-name"
+            className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 border-2 rounded-md"
+          ></input>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+        <div>
+          <label
+            htmlFor="book-author"
+            className=" text-sm font-medium text-grey-700"
+          >
+            Book Author
+          </label>
+          <input
+            type="text"
+            name="book-author"
+            id="book-author"
+            autoComplete="given-name"
+            className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 border-2 rounded-md"
+          ></input>
+        </div>
+        <div>
+          <div>
+            <label
+              htmlFor="genre"
+              className=" text-sm font-medium text-grey-700"
+            >
+              Genre
+            </label>
+            <input
+              type="text"
+              name="genre"
+              id="genre"
+              autoComplete="given-name"
+              className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 border-2 rounded-md"
+            ></input>
+          </div>
+        </div>
+        <div className="sm:col-span-2 ">
+          <button
+            type="submit"
+            className="w-full inline-flex items-center justify-center px-6 py-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
