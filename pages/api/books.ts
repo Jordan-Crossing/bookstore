9c6ca41971f85bd5ab7bb5930db49a5795eecef2
Jordan-Crossing <80609165+Jordan-Crossing@users.noreply.github.com>
2022-10-16 19:32:04 +1100

    setup db

---
+++ pages/api/books.ts
@@ -0,0 +1,53 @@
+// First we define and instantiate our Prisma client.
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+// We create a function called Handler which checks that the request method is POST,
+// then calls the function which actually writes to our database,
+// createInquiry.If the method is not POST,
+// it sends back a response with a status code of 405 and
+// a message letting the user know that this operation is not allowed.
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    return await addBook(req, res);
+  } else if (req.method === "GET") {
+    return await readBooks(req, res);
+  } else {
+    return res
+      .status(405)
+      .json({ message: "Method not allowed", success: false });
+  }
+}
+
+async function readBooks(req, res) {
+  try {
+    const allBooks = await prisma.bookSuggestion.findMany();
+    return res.status(200).json(allBooks);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ error: "Error reading from database", success: false });
+  }
+}
+
+// We define an asynchronous function, createInquiry,
+// which will take the data in the request body and send it to our database.
+async function addBook(req, res) {
+  const body = req.body;
+  // Wrapped in a try/catch block, we define a new variable newEntry and
+  // create a new entry in our Inquiry table.
+  try {
+    const newEntry = await prisma.bookSuggestion.create({
+      data: {
+        bookTitle: body.bookTitle,
+        bookAuthor: body.bookAuthor,
+        bookGenre: body.bookGenre,
+      },
+    });
+    return res.status(200).json(newEntry, { success: true });
+  } catch (error) {
+    console.error("Request error", error);
+    res.status(500).json({ error: "Error creating question", success: false });
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,71 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const [bookTitle, setBookTitle] = useState("");
+  const [bookAuthor, setBookAuthor] = useState("");
+  const [bookGenre, setBookGenre] = useState("");
+  const [APIResponse, setAPIResponse] = useState(null);
+
+  useEffect(() => {
+    console.log("bookTitle:", bookTitle);
+    console.log("book author:", bookAuthor);
+    console.log("book genre:", bookGenre);
+    console.log("API Response:", APIResponse);
+  }, [bookAuthor, bookGenre, bookTitle, APIResponse]);
+
+  const readDB = async () => {
+    try {
+      const response = await fetch("/api/books", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      setAPIResponse(await response.json());
+      if (response.status !== 200) {
+        console.log("something went wrong");
+        //set an error banner here
+      } else {
+        console.log("form submitted successfully !!!");
+        //set a success banner here
+      }
+    } catch (error) {
+      console.log("there was an error reading db", error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const body = { bookAuthor, bookGenre, bookTitle };
+    try {
+      const response = await fetch("/api/books", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      if (response.status !== 200) {
+        console.log("something went wrong");
+        //set an error banner here
+      } else {
+        resetForm();
+        readDB();
+        console.log("form submitted successfully !!!");
+        //set a success banner here
+      }
+      //check response, if success is false, dont take them to success page
+    } catch (error) {
+      console.log("there was an error submitting", error);
+    }
+  };
+
+  const resetForm = () => {
+    setBookTitle("");
+    setBookAuthor("");
+    setBookGenre("");
+  };
+
   return (
     <div className="text-center py-4 px-4">
       <h1 className="text-sky-400">BookStore</h1>
@@ -10,6 +73,7 @@ const Home: NextPage = () => {
       <form
         action="#"
         method="POST"
+        onSubmit={(e) => handleSubmit(e)}
         className=" grid grid-cols1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
       >
         <div>
@@ -20,6 +84,7 @@ const Home: NextPage = () => {
             Book Title
           </label>
           <input
+            onChange={(e) => setBookTitle(e.target.value)}
             type="text"
             name="book-title"
             id="book-title"
@@ -35,6 +100,7 @@ const Home: NextPage = () => {
             Book Author
           </label>
           <input
+            onChange={(e) => setBookAuthor(e.target.value)}
             type="text"
             name="book-author"
             id="book-author"
@@ -51,6 +117,7 @@ const Home: NextPage = () => {
               Genre
             </label>
             <input
+              onChange={(e) => setBookGenre(e.target.value)}
               type="text"
               name="genre"
               id="genre"
@@ -68,6 +135,11 @@ const Home: NextPage = () => {
           </button>
         </div>
       </form>
+      <div>
+        {APIResponse?.map((book) => (
+          <li>{book.bookTitle}</li>
+        ))}
+      </div>
     </div>
   );
 };
